782fcf563e83421d4cdc4d5ebb8fc1da5836d476
iamNavjotSingh <77809864+iamNavjotSingh@users.noreply.github.com>
2021-02-25 15:02:49 -0800

    Add files via upload

---
+++ NavjotMidTerm/src/main/java/com/example/NavjotMidTerm/NavjotMidTermApplication.java
@@ -0,0 +1,13 @@
+package com.example.NavjotMidTerm;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NavjotMidTermApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NavjotMidTermApplication.class, args);
+	}
+
+}
